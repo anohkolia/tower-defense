5fc8a05cba783307cb31b8e1297ea41954b23393
anohkolia <colectionmac.com>
2024-12-03 18:14:59 +0100

    jjjj

--- src/game/Enemy.java
+++ src/game/Enemy.java
@@ -6,9 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-
 import ui.StdDraw;
-
 public class Enemy {
     private double x, y; // Position de actuelle l'ennemi
     private  int pv;
